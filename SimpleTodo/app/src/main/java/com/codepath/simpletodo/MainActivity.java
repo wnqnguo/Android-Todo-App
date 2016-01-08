4e3e60b6dcba3c10bd414f7849e1dc5ecd14d3eb
Wenqian Guo <wnqnguo@gmail.com>
2016-01-08 14:45:18 -0800

    'delete'

--- SimpleTodo/app/src/main/java/com/codepath/simpletodo/MainActivity.java
+++ SimpleTodo/app/src/main/java/com/codepath/simpletodo/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         readItems();
         itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
+        setupListViewListener();
 
     }
 
