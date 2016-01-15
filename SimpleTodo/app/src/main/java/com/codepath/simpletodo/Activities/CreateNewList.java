522bf253282a875ff9fdf4503d3b002981fb9032
Wenqian Guo <wnqnguo@gmail.com>
2016-01-14 20:54:39 -0800

    'floating-label'

--- SimpleTodo/app/src/main/java/com/codepath/simpletodo/Activities/CreateNewList.java
+++ SimpleTodo/app/src/main/java/com/codepath/simpletodo/Activities/CreateNewList.java
@@ -1,15 +1,34 @@
 package com.codepath.simpletodo.Activities;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import com.codepath.simpletodo.Models.List;
+import com.codepath.simpletodo.Models.Task;
 import com.codepath.simpletodo.R;
 
+import java.util.ArrayList;
+
 public class CreateNewList extends AppCompatActivity {
+
+    private TextInputLayout inputLayoutTaskName;
+    private EditText inputTaskName;
+    ArrayList<Task> Tasks;
+    ArrayAdapter<Task> tasksAdapter;
+    ListView lvTasks;
+    private final int REQUEST_CODE = 20;
     private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +36,9 @@ public class CreateNewList extends AppCompatActivity {
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+        inputLayoutTaskName = (TextInputLayout) findViewById(R.id.input_layout_name);
+        inputTaskName = (EditText) findViewById(R.id.input_name);
+        inputTaskName.addTextChangedListener(new MyTextWatcher(inputTaskName));
     }
 
     @Override
@@ -39,5 +61,25 @@ public class CreateNewList extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    private class MyTextWatcher implements TextWatcher {
+
+        private View view;
+
+        private MyTextWatcher(View view) {
+            this.view = view;
+        }
+
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void afterTextChanged(Editable editable) {
+            switch (view.getId()) {
+
+            }
+        }
     }
 }
