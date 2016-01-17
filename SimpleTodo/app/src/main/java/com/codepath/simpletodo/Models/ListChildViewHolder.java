f642bb8c883b5448d600b0ab1adb061a412bd1d5
Wenqian Guo <wnqnguo@gmail.com>
2016-01-16 18:34:52 -0800

    'adding-xml-for-list-task'

---
+++ SimpleTodo/app/src/main/java/com/codepath/simpletodo/Models/ListChildViewHolder.java
@@ -0,0 +1,7 @@
+package com.codepath.simpletodo.Models;
+
+/**
+ * Created by wguo on 1/16/2016.
+ */
+public class ListChildViewHolder {
+}
---
+++ SimpleTodo/app/src/main/java/com/codepath/simpletodo/Models/ListParentViewHolder.java
@@ -0,0 +1,10 @@
+package com.codepath.simpletodo.Models;
+
+import com.bignerdranch.expandablerecyclerview.ViewHolder.ParentViewHolder;
+
+/**
+ * Created by wguo on 1/16/2016.
+ */
+public class ListParentViewHolder extends ParentViewHolder {
+
+}
